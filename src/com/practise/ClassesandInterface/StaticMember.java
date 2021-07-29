ceee45483a87ba340d67143f0ed86ab2b553b19b
nazeerahmedoffical <nazeerahmedofficial1@gmail.com>
2021-07-29 12:26:47 +0500

    working with static member and metho

--- src/com/practise/ClassesandInterface/StaticMember.java
+++ src/com/practise/ClassesandInterface/StaticMember.java
@@ -1,4 +1,21 @@
 package com.practise.ClassesandInterface;
 
 public class StaticMember {
+    private int passneger;
+     static int totalPassenger; // static same for all instance
+    private static int seats=3;
+    // static initilazer
+//    static
+//    {
+//
+//    }
+    public void addPassenger()
+    {
+       passneger++;
+       totalPassenger++;
+    }
+    public static  int gets()
+    {
+        return seats;
+    }
 }
